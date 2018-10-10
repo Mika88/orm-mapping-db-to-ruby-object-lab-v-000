5e41338703a96f4f8caa0a7cd69e6a487d32520d
Mika88 <mikaspivak@gmail.com>
2018-10-10 15:24:59 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -69,8 +69,9 @@ class Student
       DB[:conn].execute(sql, 10, x).map do |row|
          self.new_from_db(row)
       end
+      binding.pry
   end
-binding.pry
+  
   def self.first_student_in_grade_10
     self.first_X_students_in_grade_10(1)
   end
