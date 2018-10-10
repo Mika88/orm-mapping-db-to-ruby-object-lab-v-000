6d27cfc66563db51916ec7b03180fdcd8726c9a5
Mika88 <mikaspivak@gmail.com>
2018-10-10 15:19:22 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -70,7 +70,9 @@ class Student
          self.new_from_db(row).name
       end
   end
-
+  def self.first_student_in_grade_10
+    self.first_X_students_in_grade_10(1)
+  end
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
