9c0bb7f5c32109d086478d1bb86a43de5454d2bf
Mika88 <mikaspivak@gmail.com>
2018-10-10 15:39:43 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -75,6 +75,17 @@ class Student
     self.first_X_students_in_grade_10(1)[0]
   end
 
+  def self.all_students_in_grade_X(x)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+      SQL
+
+      DB[:conn].execute(sql, x).map do |row|
+         self.new_from_db(row)
+  end
+  
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
